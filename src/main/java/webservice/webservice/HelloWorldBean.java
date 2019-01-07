dc66c6c5beb5b5e0523c1a1c3bdd21e2b30d6afb
neelamlamba85 <neelamlamba85@gmail.com>
2019-01-07 10:13:01 -0600

    first commit

---
+++ src/main/java/webservice/webservice/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package webservice.webservice;
+
+public class HelloWorldBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public HelloWorldBean(String message) {
+      this.message=message;
+    }
+
+
+}
